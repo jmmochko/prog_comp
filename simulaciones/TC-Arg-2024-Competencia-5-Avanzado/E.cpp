b3d56c12af960e4133108ca2fd9681c237fc357e
Mateo Ricci <82452738+MateoRicci@users.noreply.github.com>
2024-07-30 11:23:17 -0300

    Update E.cpp

--- simulaciones/TC-Arg-2024-Competencia-5-Avanzado/E.cpp
+++ simulaciones/TC-Arg-2024-Competencia-5-Avanzado/E.cpp
@@ -1,97 +1,65 @@
 #include <bits/stdc++.h>
-#define FIN ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
-#define pb push_back
-#define fore(a,b,c) for(int a=b; a<c; ++a)
-#define dfore(a,b,c) for(int a=b; a>=c; --a)
-#define SZ(a) ((int)a.size())
 #define fst first
 #define snd second
-#define show(a) cout<<a<<"\n"
-#define showAll(a) for(auto i:a) cout<<i<<" ";cout<<"\n"
-#define input(a) for(auto& i:a) cin>>i
-#define all(a) a.begin(),a.end()
-#define DGB(a) cout<<#a<<" = "<<a<<"\n"
-#define RAYA cout<<"=============="<<"\n"
-#define pii pair<int,int>
-#define pll pair<ll,ll>
-#define MAXN 100005
-#define ALPH 26
-#define M 1000000007
-#define MAXINT (1<<30)
-#define MAXll (1ll<<60)
-#define PI 3.141592653
+#define fore(i,a,b) for(ll i=a,ThxDem=b;i<ThxDem;++i)
+#define pb push_back
+#define ALL(s) s.begin(),s.end()
+#define FIN ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define SZ(s) ll(s.size())
 using namespace std;
 typedef long long ll;
-typedef unsigned int ui;
-typedef unsigned long long ull;
+typedef pair<ll,ll> ii;
 
-//El Vasito is love, El Vasito is life
-struct UF {
-	vector<int> e;
-	UF(int n) : e(n, -1) {}
-	bool sameSet(int a, int b) { return find(a) == find(b); }
-	int size(int x) { return -e[find(x)]; }
-	int find(int x) { return e[x] < 0 ? x : e[x] = find(e[x]); }
-	bool join(int a, int b) {
-		a = find(a), b = find(b);
-		if (a == b) return false;
-		if (e[a] > e[b]) swap(a, b);
-		e[a] += e[b]; e[b] = a;
-		return true;
-	}
-};
+void bfs(ll v, vector<set<ll>> &g, set<ll> &unvis, vector<bool> &vis) {
 
-vector<int> g[MAXN];
+    vis[v] = true;
 
-void solve(){
-    int n,m;
-    cin>>n>>m;
-    fore(i,0,m){
-        int u,v;
-        cin>>u>>v;
-        --u;--v;
-        g[u].push_back(v);
-        g[v].push_back(u);
-    }
-    if(m<n-1||m==0){
-        show(0);
-        return;
-    }
-    fore(i,0,n)sort(all(g[i]));
-    int cmps = 1;
-    UF uf(n);
-    uf.e[0]=1;
-    fore(i,1,n){
-        int j = 0;
-        ++cmps;
-        vector<int> cnt(cmps+1,0),exs(cmps+1);
-        while(g[i][j]<i){
-            int u = g[i][j];
-            int c = uf.find(u);
-            cnt[c]++;
-            exs[c] = u;
-            ++j;
-        }
-        DGB(i);
-        showAll(cnt);
-        fore(k,0,cmps){
-            if(cnt[k]<uf.size(exs[k])){
-                DGB(exs[k]);
-                uf.join(exs[k],i);
-                --cmps;
+    queue<ll> q,td;
+    q.push(v);
+
+    while (!q.empty()) {
+
+        v = q.front();
+        q.pop();
+        
+        for (ll u : unvis) {
+
+            if (!vis[u] && g[v].find(u) == g[v].end()) {
+                
+                td.push(u);
+		        vis[u] = true;
+			    q.push(u);
+
             }
         }
-        showAll(uf.e);
+
+        while(!td.empty()) unvis.erase(td.front()), td.pop(); // optimizacion para no repetir los no visitdaos
     }
-    show(cmps-1);
 }
+
 
 int main(){
-    //FIN;
-    int t = 1;
-    //cin>>t;
-    while(t--)solve();
-    return 0;
+    FIN;
+    ll n,m; cin >> n >> m;
+    vector<set<ll>> g(n);
+    fore(i,0,m){
+        ll a,b; cin >> a >> b; a--;b--;
+        g[a].insert(b);
+        g[b].insert(a);
+    }
+    vector<bool> vis(n,false);
+    set<ll> unvis;
+    fore(i,0,n){
+        unvis.insert(i);
+    }
+    ll res = 0;
+    fore(i,0,n){
+        if(!vis[i]){
+            bfs(i,g,unvis,vis);
+            res++;
+        }
+    }
+    cout << res-1 << "\n";
 }
 
-//Escuchen, corran la bola...
+// Gallardo vuelve a river
