6e65b4251bdad296cf8d6a8b0aafb486119a7bf2
leovilca <65786858+leovilca@users.noreply.github.com>
2024-08-02 16:57:45 -0300

    Update J.cpp

---
+++ simulaciones/TC-Arg-2024-Competencia-2-Inicial/J.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define FIN ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define fst first
+#define snd second
+typedef long long ll;
+typedef unsigned int ui;
+typedef unsigned long long ull;
+#define pb push_back
+#define SZ(a) ((int)a.size())
+#define fore(a,b,c) for(int a=b; a<c; ++a)
+#define dfore(a,b,c) for(int a=b; a>=c; --a)
+#define show(a) cout<<a<<"\n"
+#define showAll(a) for(auto i:a) cout<<i<<" ";cout<<"\n"
+#define input(a) for(auto& i:a) cin>>i
+#define all(a) a.begin(),a.end()
+#define DBG(a) cout<<#a<<" = "<<a<<"\n"
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define jump cout<<"\n"
+#define RAYA cout<<"=============="<<"\n"
+#define MAXN 200005
+#define ALPH 26
+#define M 1000000007
+#define MAXINT (1<<30)
+#define MAXll (1<<60)
+#define PI 3.141592653
+
+int main(){
+    FIN;
+    //ifstream cin("input.txt");
+    ll t; cin>>t;
+    while(t--){
+        ll a,b;
+        cin>>a>>b;
+        show(a*b);
+    }
+    //system("pause");
+    //cin.close()
+    return 0;
+}
