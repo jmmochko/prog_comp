c6224b0ac3cc0756578bb80ed6db90dad2331c78
Mateo Ricci <82452738+MateoRicci@users.noreply.github.com>
2025-03-24 09:37:51 -0300

    Update E.cpp

---
+++ simulaciones/GranPremioMX1rafecha/E.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define fst first
+#define snd second
+#define fore(i,a,b) for(ll i=a,ThxDem=b;i<ThxDem;++i)
+#define dfore(a,b,c) for(int a=b; a>=c; --a)
+#define DGB(a) cout<<#a<<" = "<<a<<"\n"
+#define RAYA cout<<"=============="<<"\n"
+#define show(a) cout<<a<<"\n"
+#define showAll(a) for(auto i:a) cout<<i<<" ";cout<<"\n"
+#define input(a) for(auto& i:a) cin>>i
+#define pb push_back
+#define ALL(s) s.begin(),s.end()
+#define FIN ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define SZ(s) ll(s.size())
+using namespace std;
+typedef long long ll;
+typedef pair<ll,ll> ii;
+
+int main(){
+    FIN;
+    ll t = 1;
+    // cin >> t;
+    while(t--){
+        ll a,b,k;
+        cin >> a >> b >> k;
+        fore(i,1,k+1){
+            cout << (a*i+b) << " ";
+        }
+        cout << "\n";
+    }
+
+}
